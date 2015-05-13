afdfe54610187f3cee96761333f3dc6b41169db7
SLoW <snollygolly@gmail.com>
2015-05-12 19:18:59 -0500

    added clubs and nations

--- schema.js
+++ schema.js
@@ -1,4 +1,90 @@
 var Schema = {
+  clubs: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    manager_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the manager who owns this club"
+    },
+    stadium_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the stadium this club belongs to"
+    },
+    division_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the division this club belongs to"
+    },
+    nation_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the nation this club belongs to"
+    },
+    name: {
+      type: 'string',
+      maxlength: 255,
+      nullable: false,
+      comment: "Name of the club"
+    }
+  },
+  nations: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    name: {
+      type: 'string',
+      maxlength: 255,
+      nullable: false,
+      comment: "Name of the nation"
+    },
+    size: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "The size of the nation"
+    },
+    duration: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "How long is an in-game day (in minutes)"
+    },
+    slug: {
+      type: 'string',
+      maxlength: 255,
+      nullable: false,
+      comment: "URL friendly/shortened name of the nation"
+    },
+    tiers: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Unknown"
+    },
+    divisions_per_tier: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Unknown"
+    },
+    clubs_per_division: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Unknown"
+    }
+  },
   players: {
     id: {
       type: 'increments',
@@ -19,19 +105,19 @@ var Schema = {
     },
     first_name: {
       type: 'string',
-      maxlength: 25,
+      maxlength: 32,
       nullable: false,
       comment: "First name of the player"
     },
     last_name: {
       type: 'string',
-      maxlength: 25,
+      maxlength: 32,
       nullable: false,
       comment: "Last name of the player"
     },
     suffix: {
       type: 'string',
-      maxlength: 25,
+      maxlength: 8,
       nullable: false,
       comment: "Jr Sr (Suffix)"
     },
@@ -61,7 +147,7 @@ var Schema = {
     },
     position: {
       type: 'string',
-      maxlength: 3,
+      maxlength: 4,
       nullable: false,
       comment: "What position does this player play"
     },
