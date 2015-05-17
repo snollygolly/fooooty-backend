685f0bd5ab8020f8974ea24ed1f9a17b76d69daf
clockworkhero <graham.j.freeman@gmail.com>
2015-05-17 15:47:09 -0400

    Update schema.js
    My first set of "real code" on github, as well as first non-trivial-within-html javascript!

--- schema.js
+++ schema.js
@@ -10,7 +10,7 @@ var Schema = {
       nullable: false,
       unsigned: true,
       comment: "ID of the manager who owns this club"
-    },
+    },	
     stadium_id: {
       type: 'integer',
       nullable: false,
@@ -22,6 +22,12 @@ var Schema = {
       nullable: false,
       unsigned: true,
       comment: "ID of the division this club belongs to"
+    },
+    divisional_position: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Position within the division this club belongs to"
     },
     nation_id: {
       type: 'integer',
@@ -53,6 +59,18 @@ var Schema = {
       nullable: false,
       unsigned: true,
       comment: "The size of the nation"
+    },
+    group_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the group this club belongs to"
+    },
+    zone_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the zone this club belongs to"
     },
     duration: {
       type: 'integer',
@@ -125,7 +143,7 @@ var Schema = {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Age of the player, in sessions?"
+      comment: "Age of the player, in sessions"
     },
     starting: {
       type: 'boolean',
@@ -137,13 +155,13 @@ var Schema = {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Skill level of the player"
     },
     super_skill: {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Unique player ability, if any"
     },
     position: {
       type: 'string',
@@ -155,37 +173,326 @@ var Schema = {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Number of matches played this session Out Of Position"
     },
     dp: {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Number of discilinary points this player has accrued"
     },
     suspended: {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Number of games this player has been suspended due to excessive DP"
     },
     health: {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Percent of full health this player has"
     },
     auction_type: {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Which auction this player is listed in, if any"
     },
     auction_price: {
       type: 'integer',
       nullable: false,
       unsigned: true,
-      comment: "Unknown"
+      comment: "Current auction price for this player"
+    }
+  },
+  botmanagers: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    botmanager_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the manager who owns this club"
+    },
+    bot_position_pref: {
+      type: 'string',
+      maxlength: 4,
+      nullable: false,
+      comment: "What position does this botmanager favor"
+    },
+    bot_OST: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Percent of games this bot will play the Offside Trap"
+    },
+    bot_sweeper: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Percent of games this bot will play a sweeper"
+    },
+    bot_hardness: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Hardness level this bot plays at"
+    }
+  },
+  defaults: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    default_zones: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of zones when creating a new instance of the game"
+    },
+    default_groups: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of groups when creating a new instance of the game"
+    },
+    default_speeds: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Default zone speeds in minutes"
+    },
+    default_tiernames: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Default names for tiers within a nation"
+    },
+    default_separators: {
+      type: 'string',
+      maxlength: 200,
+      nullable: false,
+      comment: "Default separator names for tiers within a nation"
+    },
+    images_path: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Path to image directory"
+    },
+    data_path: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Path to data directory"
+    }
+  },
+  divisions: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    division_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the division"
+    },
+    division_name: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Full name of the division"
+    },
+    division_tier: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of the tier this division is in"
+    },
+    division_depth: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Depth within the tier of this division"
+    },
+    division_promotion_to: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the next division top teams from this division will be promoted to"
+    },
+    division_number_of_clubs: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of clubs in this division"
+    },
+    division_regionalLocation: {
+      type: 'string',
+      maxlength: 13,
+      nullable: false,
+      comment: "Breakdown of exact geographical location within nation of this division"
+    }
+  },
+  groups: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    group_id: {
+      type: 'string',
+	  maxlength: 4,
+      nullable: false,
+      comment: "ID letter of the group"
+    },
+    group_name: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Full name of the group"
+    }
+  },
+  zones: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    zone_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the zone left to right quick to slow"
+    },
+    zone_name: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Full name of the zone"
+    }
+  },
+  stadiums: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    nation_id: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "ID of the nation who owns this stadium"
+    },
+    stadium_regionalLocation: {
+      type: 'string',
+      maxlength: 13,
+      nullable: false,
+      comment: "Breakdown of exact geographical location within nation of this stadium"
+    },
+    stadium_grass: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Percent health of playing area grass"
+    },
+    stadium_lighting: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Floodlight level of this stadium"
+    },
+    stadium_media: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Quality of media access areas of this stadium"
+    },
+    stadium_parking: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of parking spots in the lot of this stadium"
+    },
+    stadium_capacity: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Maximum number of fans this stadium can hold"
+    }
+  },
+  users: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true
+    },
+    username: {
+      type: 'string',
+      maxlength: 24,
+      nullable: false,
+      comment: "Display name of this user"
+    },
+    user_email: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Email address of this user"
+    },
+    user_passwordHash: {
+      type: 'string',
+      maxlength: 64,
+      nullable: false,
+      comment: "Hash value of password of this user"
+    },
+    user_realname: {
+      type: 'string',
+      maxlength: 100,
+      nullable: false,
+      comment: "Real name of this user"
+    },
+    user_bio: {
+      type: 'string',
+      maxlength: 1000,
+      nullable: false,
+      comment: "Public bio text input by this user"
+    },
+    user_status: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Whether user is active dormant manager director or chairman"
+    },
+    user_credits: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of credits in this users account"
+    },
+    user_revert: {
+      type: 'dateTime',
+      nullable: false,
+      unsigned: true,
+      comment: "Date and time at which this user account reverts back to manager from purchased director or chairman"
+    },
+    user_visas: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+      comment: "Number of travel visas in this users account"
+    },
+    user_cookieAccept: {
+      type: 'dateTime',
+      nullable: false,
+      unsigned: true,
+      comment: "Date and time at which this user last accepted the cookie notice"
     }
   }
 };
