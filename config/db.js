8f952c3a5ebfbf5077f7b339de9dd46b5408a9ba
SLoW <snollygolly@gmail.com>
2015-05-26 18:02:11 -0500

    add DB config for production

--- config/db.js
+++ config/db.js
@@ -1,13 +1,26 @@
-var DBConfig = {
-  client: 'mysql',
-  connection: {
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : 'root',
-    database : 'football',
-    charset  : 'utf8'
-  }
-};
+if (process.env.NODE_ENV == "production"){
+  var DBConfig = {
+    client: 'mysql',
+    connection: {
+      host     : '127.0.0.1',
+      user     : 'root',
+      password : 'kCRipSLVG0',
+      database : 'football',
+      charset  : 'utf8'
+    }
+  };
+} else {
+  var DBConfig = {
+    client: 'mysql',
+    connection: {
+      host     : '127.0.0.1',
+      user     : 'root',
+      password : 'root',
+      database : 'football',
+      charset  : 'utf8'
+    }
+  };
+}
 
 var knex = require('knex')(DBConfig);
 var bookshelf = require('bookshelf')(knex);
