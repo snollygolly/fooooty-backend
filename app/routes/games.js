6593e6b967eb35b3b105f085dc510c6b01d2b697
SLoW <snollygolly@gmail.com>
2015-05-28 19:43:12 -0500

    more unfinished code

--- app/routes/games.js
+++ app/routes/games.js
@@ -33,7 +33,7 @@ var Game = {
 	joinGameAsHomeClub: function (req, res) {
 		var gameId = req.params.id;
 		var clubId = parseInt(req.params.cid);
-		new Model.Division()
+		new Model.Game()
 			.where('id', gameId)
 			.fetch()
 			.then(function (model) {
@@ -44,13 +44,13 @@ var Game = {
 				console.log(error);
 				res.send('An error occured');
 			});
-	}
+	},
 
 	/* Join away club */
 	joinGameAsAwayClub: function (req, res) {
 		var gameId = req.params.id;
 		var clubId = parseInt(req.params.cid);
-		new Model.Division()
+		new Model.Game()
 			.where('id', gameId)
 			.fetch()
 			.then(function (model) {
@@ -61,24 +61,41 @@ var Game = {
 				console.log(error);
 				res.send('An error occured');
 			});
-	}
+	},
 
 	/* Schedule a game for a day */
 	scheduleGameForDay: function (req, res) {
 		var gameId = req.params.id;
 		var dayId = parseInt(req.params.did);
-		new Model.Division()
+		new Model.Game()
 			.where('id', gameId)
 			.fetch()
 			.then(function (model) {
 				model.set('day_id', dayId);
+				model.set('status', "SCHEDULED");
 				model.save();
 				res.json(model);
 			}).catch(function (error) {
 				console.log(error);
 				res.send('An error occured');
 			});
-	}
+	},
+
+	/* Kickoff a game simulation */
+	simulateGame: function (req, res) {
+		var gameId = req.params.id;
+		new Model.Game()
+			.where('id', gameId)
+			.fetch()
+			.then(function (model) {
+				model.set('away_club_id', clubId);
+				model.save();
+				res.json(model);
+			}).catch(function (error) {
+				console.log(error);
+				res.send('An error occured');
+			});
+	},
 
 	/* Create a game */
 	createGame: function (req, res) {
@@ -92,6 +109,14 @@ var Game = {
 			});
 	}
 };
+
+function
+
+function generateGameJSON(game){
+	var returnObj = {};
+	returnObj.recap = [];
+	returnObj.recap.push("Game has been simulated!");
+}
 
 
 /* Exports all the functions */
